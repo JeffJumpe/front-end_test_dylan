7ef882d19224fc7c5bd8a5c6024a9928685b7378
dintorf <dintorf@gmail.com>
2017-03-08 14:21:18 -0700

    Added react-d3 for easier interaction between react and d3

--- front-end-test-dylan/src/App.js
+++ front-end-test-dylan/src/App.js
@@ -1,19 +1,63 @@
 import React, { Component } from 'react';
+import BarChart from 'react-d3-basic';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    var generalChartData = [
+      {
+        letter: "A",
+        frequency: .08167
+      },
+      {
+        letter: "B",
+        frequency: .01492
+      },
+      {
+        letter: "C",
+        frequency: .02782
+      },
+      {
+        letter: "D",
+        frequency: .04253
+      },
+      {
+        letter: "E",
+        frequency: .12702
+      }
+    ]
+
+    var width = 700,
+    height = 400,
+    title = "Bar Chart",
+    chartSeries = [
+      {
+        field: 'frequency',
+        name: 'Frequency'
+      }
+    ],
+    x = function(d) {
+      return d.letter;
+    },
+    xScale = 'ordinal',
+    xLabel = "Letter",
+    yLabel = "Frequency",
+    yTicks = [10, "%"];
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BarChart
+        title= {title}
+        data= {generalChartData}
+        width= {width}
+        height= {height}
+        chartSeries = {chartSeries}
+        x= {x}
+        xLabel= {xLabel}
+        xScale= {xScale}
+        yTicks= {yTicks}
+        yLabel = {yLabel}
+      />
     );
   }
 }
